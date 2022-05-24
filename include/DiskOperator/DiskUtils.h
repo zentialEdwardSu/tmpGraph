b5343cd6c168791c0a6a98febcab17e2e40a0028
zentialEdwardSu <zentialeds@gmail.com>
2022-05-24 23:07:08 +0800

    Create Disk operator utils

---
+++ include/DiskOperator/DiskUtils.h
@@ -0,0 +1,9 @@
+//
+// Created by lin on 5/24/2022.
+//
+
+#ifndef TMPGRAPH_DISKUTILS_H
+#define TMPGRAPH_DISKUTILS_H
+
+#include"GraphStruct.h"
+#endif //TMPGRAPH_DISKUTILS_H
---
+++ src/DiskOperator/DiskUtils.c
@@ -0,0 +1,9 @@
+/**
+ * @author EdwardSu
+ * @file DiskUtils.c
+ * @brief DiskUtils function
+ */
+#include<stdio.h>
+#include<stdlib.h>
+#include"DiskUtils.h"
+
